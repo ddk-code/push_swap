2a419c0e95ea7ec34330cf658bc186e207340dbe
Principal Charlotte <pcharlot@il-h4.21-school.ru>
2020-02-09 21:18:08 +0300

    Учить листы срочно...

--- push_swap.c
+++ push_swap.c
@@ -12,9 +12,24 @@
 
 #include "push_swap.h"
 
+void	set_value_to_stack(t_stack **stack, int nbr)
+{
+	t_stack	*temp;
+	t_stack *head;
+
+	temp = stack;
+
+
+
+}
+
+
+
 int 	main(int argc, char **argv)
 {
 	char	*s;
+	int		i;
+	char	*sp;
 
 	if (argc <= 1)
 	{
@@ -22,10 +37,20 @@ int 	main(int argc, char **argv)
 		return (0);
 	}
 	s = malloc(0);
+//	printf("len s = %d\n", ft_strlen(s));
+//	s = ft_strjoin(s, argv[1]);
+//	printf("s = %s\n", s);
+//	printf("len s = %d\n", ft_strlen(s));
 
-	printf("len s = %d\n", ft_strlen(s));
-	s = ft_strjoin(s, argv[1]);
-	printf("s = %s\n", s);
-	printf("len s = %d\n", ft_strlen(s));
-	
+	i = 1;
+	sp = malloc(2);
+	sp[0] = ' ';
+	sp[1] = '\0';
+	while (argc > i)
+	{
+		s = ft_strjoin(s, argv[i]); ///все аргументы сливаем в одну строку
+		s = ft_strjoin(s, sp); ///разделяем аргументы пробелом
+		++i;
+	}
+//	printf("s = %s\n", s);
 }
