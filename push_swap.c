9c34b7db0375efdf651e8143cfeb2f4a7cb1c7cb
Principal Charlotte <pcharlot@il-k6.21-school.ru>
2020-02-15 16:41:06 +0300

    isspace

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pcharlot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 16:46:24 by pcharlot          #+#    #+#             */
-/*   Updated: 2020/02/15 15:45:15 by pcharlot         ###   ########.fr       */
+/*   Updated: 2020/02/15 16:34:06 by pcharlot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,67 +24,60 @@ void	set_value_to_stack(t_stack **stack, int nbr)
     }
 }
 
-//void    set_stack()
-//{
-//
-//}
-
-int 	par_number(char *s)
+int intnum(char *s)
 {
-	int	i;
-	int	n;
+	int i;
 
 	i = 0;
 	while(s[i])
 	{
-		if (is_digit09(s[i]))
-		{
-			n = ft_atoi(s);
-			i = i + ft_digcount(n);
-		}
-
+		if(is_digit09(s[i]) &&)
 	}
 }
 
+int *tab(char *s)
+{
+
+}
 
 int 	main(int argc, char **argv)
 {
-//	char	*s;
-//	int		i;
-//	char	*sp;
-//
-//	if (argc <= 1)
-//	{
-//		printf("Аргументов недостаточно");
-//		return (0);
-//	}
-//	s = malloc(0);
-////	printf("len s = %d\n", ft_strlen(s));
-////	s = ft_strjoin(s, argv[1]);
-////	printf("s = %s\n", s);
-////	printf("len s = %d\n", ft_strlen(s));
-//
-//	i = 1;
-//	sp = malloc(2);
-//	sp[0] = ' ';
-//	sp[1] = '\0';
-//	while (argc > i)
-//	{
-//		s = ft_strjoin(s, argv[i]); ///все аргументы сливаем в одну строку
-//		s = ft_strjoin(s, sp); ///разделяем аргументы пробелом
-//		++i;
-//	}
+	char	*s;
+	int		i;
+	char	*sp;
+
+	if (argc <= 1)
+	{
+		printf("Аргументов недостаточно");
+		return (0);
+	}
+	s = malloc(0);
+//	printf("len s = %d\n", ft_strlen(s));
+//	s = ft_strjoin(s, argv[1]);
 //	printf("s = %s\n", s);
+//	printf("len s = %d\n", ft_strlen(s));
+
+	i = 1;
+	sp = malloc(2);
+	sp[0] = ' ';
+	sp[1] = '\0';
+	while (argc > i)
+	{
+		s = ft_strjoin(s, argv[i]); ///все аргументы сливаем в одну строку
+		s = ft_strjoin(s, sp); ///разделяем аргументы пробелом
+		++i;
+	}
+	printf("s = %s\n", s);
 
 
 //	printf("%d: %d\n", -156, ft_digcount(-156));
 //	printf("%d: %d\n", 0, ft_digcount(0));
 
 
-	char *inmi = "-2147483649";
-	char *inma = "2147483647";
-	printf("%ld\n", ft_atoi(inmi));
-	printf("%ld\n", ft_atoi(inma));
+//	char *inmi = "-2147483649";
+//	char *inma = "2147483647";
+//	printf("%ld\n", ft_atoi(inmi));
+//	printf("%ld\n", ft_atoi(inma));
 
 	return (0);
 }
